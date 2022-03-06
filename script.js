7fcf683d4a81a09fc4beb1439de316eb353403dc
ejwalters <ejwalters24@gmail.com>
2022-03-06 08:02:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function send(method) {
       break;
     case "login":
       analytics.identify("43259820234", {
-        name: 'Eric Walters',
-        email: 'ejwalters24@gmail.com',
-        favoriteTeam: 'Cleveland Browns'
+        name: 'Ann Walters',
+        email: 'amjesko@gmail.com',
+        favoriteTeam: 'Cleveland Cavaliers'
       });
       break;
     case "sneaker":
